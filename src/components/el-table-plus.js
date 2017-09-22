0eed32604a9ec829a6f98725397397c660cc19b0
Chikara Chan <771334038@qq.com>
2017-09-22 17:47:26 +0800

    feat: add pagination align support

--- src/components/el-table-plus.js
+++ src/components/el-table-plus.js
@@ -1,9 +1,8 @@
 export default {
   name: 'ElTablePlus',
+  componentName: 'ElTablePlus',
   props: {
-    currentChangeAsync: {
-      type: Function
-    },
+    currentChangeAsync: Function,
     columns: {
       type: Array,
       default: []
@@ -11,6 +10,11 @@ export default {
     pageSize: {
       type: Number,
       default: 20
+    },
+    paginationAlign: {
+      type: String,
+      default: 'center',
+      validator: val => ['left', 'right', 'center'].includes(val)
     }
   },
   data() {
@@ -30,61 +34,74 @@ export default {
     reload() {
       this.handleCurrentChange(1);
     },
+    reloadCurrent() {
+      this.getData();
+    },
     handleCurrentChange(currentPage) {
       this.currentPage = currentPage;
       this.getData();
     },
     async getData() {
+      this.data = [];
       this.loading = true;
+      try {
+        const {data, total} = await this.currentChangeAsync(this.currentPage, this.pageSize);
 
-      const {data, total} = await this.currentChangeAsync(this.currentPage, this.pageSize);
-
-      this.data = data;
-      this.total = total;
+        this.data = data;
+        this.total = total;
+      } catch (e) {
+        this.$emit('error', e);
+      }
       this.loading = false;
     }
   },
   render(h) {
+    const namespace = 'el-table-plus';
+    const { caption, actionBar, ...otherSlots } = this.$slots;
+
     return (
-      <div class="el-table-plus">
+      <div class={namespace}>
         {
-          (this.$slots.caption || this.$slots.actionBar) &&
-          <div class="el-table-plus__header">
-            <h1 class="el-table-plus__header__caption">{this.$slots.caption}</h1>
-            <div class="el-table-plus__header__action-bar">{this.$slots.actionBar}</div>
+          (caption || actionBar) &&
+          <div class={`${namespace}__header`}>
+            <h1 class={`${namespace}__header__caption`}>{caption}</h1>
+            <div class={`${namespace}__header__action-bar`}>{actionBar}</div>
           </div>
         }
-        <el-table ref="table" data={this.data} v-loading={this.loading} {...{props: this.$attrs}}
-          {...{on: this._events}}>
+        <el-table data={this.data} v-loading={this.loading}
+          {...{ props: this.$attrs, on: this.$listeners }}>
+          {
+            Object.entries(otherSlots).map(([key, val]) =>
+              <template slot={key}>{val}</template>
+            )
+          }
           {
             this.columns.map(column => {
-              const nextColumn = {
-                ...column,
-                formatter: column.formatter && (row => column.formatter(row[column.prop]))
+              const { formatter, renderBody } = column;
+              const data = {
+                props: {
+                  ...column,
+                  formatter: formatter && (row => formatter(row[column.prop]))
+                }
               };
 
-              return nextColumn.renderBody
-                ? (
-                  <el-table-column
-                    {...{props: nextColumn}}
-                    scopedSlots={
-                      {
-                        default({row}) {
-                          return nextColumn.renderBody(h, row);
-                        }
-                      }
-                    }>
-                  </el-table-column>
-                ) : (
-                  <el-table-column {...{props: nextColumn}}>
-                  </el-table-column>
-                );
+              if (renderBody) {
+                data.scopedSlots = {
+                  default({ row }) {
+                    return renderBody(h, row);
+                  }
+                };
+              }
+              return (
+                <el-table-column {...data}></el-table-column>
+              );
             })
           }
         </el-table>
         {
           this.hasPagination &&
-          <el-pagination current-page={this.currentPage} page-size={this.pageSize}
+          <el-pagination class={`el-pagination--${this.paginationAlign}`}
+            current-page={this.currentPage} page-size={this.pageSize}
             total={this.total} on-current-change={this.handleCurrentChange}
             layout="total, prev, pager, next">
           </el-pagination>
